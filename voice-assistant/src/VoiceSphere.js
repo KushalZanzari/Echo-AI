061593b1ec370ed80008dffcd698c30acf3b316f
Kushal Zanzari <146122165+KushalZanzari@users.noreply.github.com>
2026-06-08 11:36:13 +0530

    Remove from VoiceSphere.js

--- voice-assistant/src/VoiceSphere.js
+++ voice-assistant/src/VoiceSphere.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useTranscript } from './TranscriptContext';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import { ArrowLeft, MicOff } from 'lucide-react';
